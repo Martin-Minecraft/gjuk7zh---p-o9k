674ff2ddde852749eb44a08ae2073200a5fb6daf
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-25 19:55:19 +0200

    Create Breaking Point.lua

---
+++ Breaking Point.lua
@@ -0,0 +1,20 @@
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/SKZjdjdj"))();
+
+
+local Gamer = library:CreateSection("Breaking Point");
+
+Gamer1:Button("first");
+
+--first
+loadstring(game:HttpGet('https://pastebin.com/raw/MLPypHYv', true))()
+
+end)
+Gamer1:Button("2nd");
+
+--2nd
+loadstring(game:HttpGet('https://pastebin.com/raw/t2fmrD3K', true))()
+
+end)
+
+
+library:Ready();
